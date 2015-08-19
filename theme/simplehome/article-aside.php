f039aa7479bd576de8826030b743a9fb716e4782
QiuH Yang <fsstyle@Hotmail.com>
2015-08-19 10:57:32 +0800

    add cha

--- theme/simplehome/article-aside.php
+++ theme/simplehome/article-aside.php
@@ -2,6 +2,27 @@
             	<h1><a href="<?php the_permalink(); ?>" title="<?php the_title();?>" alt="<?php the_title();?>"><?php the_title() ?></a></h1>
             	<div class="aside">
                    <?php the_content(); ?>
+				   <?php
+						if (get_post_meta($post->ID, "music_url_value", true)) {
+						?>
+						<!-- music format -->
+						<div class="post-format-audio">
+							<div class="feature-img audio">
+							<?php if ( has_post_thumbnail() ) { ?>
+								<a href="<?php the_permalink(); ?>" title="<?php the_title();?>" alt="<?php the_title();?>"><?php the_post_thumbnail(); ?></a>
+							  <?php } ?>  
+							   <div class="audio-wrapper">
+										<div class="me-wrap">
+										<audio class="wp-audio-shortcode" preload="none" style="width: 100%">
+										<source type="audio/mp3" src="<?php echo get_post_meta( $post->ID, 'music_url_value', true ); ?>">
+										</audio>
+										</div>
+									</div>	
+						</div>
+						</div>
+					<?php
+						}
+					?>
                 </div>
                 <div class="aside-info"><?php the_time("l");?>
                 <?php 
